8692cbdb84ef0adc108b7ed3327c27482cffeb02
Max Polzin <polzin.max@gmail.com>
2024-05-08 16:05:24 +0200

    Update run script.

--- run.sh
+++ run.sh
@@ -4,6 +4,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 export HOST_UID=$(id -u)
 
-docker-compose -f $SCRIPT_DIR/docker-compose.yml run \
+docker compose -f $SCRIPT_DIR/docker-compose.yml run \
 --volume $SCRIPT_DIR/rslidar_sdk:/colcon_ws/src/rslidar_sdk \
-ros-encoder bash
+--volume $SCRIPT_DIR/rslidar_msg:/colcon_ws/src/rslidar_msg \
+ros-rslidar-sdk bash
